9f9110a283acb90fc60c437aabc4b68c2a9fd5d1
P <dpheeraphat2547@gmail.com>
2022-12-20 20:39:57 +0700

    State
    State&Usestate

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 const FormComponent = () => {
+    const [userName,setUserName] = useState('');
+    const [email,SetEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmpassword,setConfirmPassword] = useState('');
     return(       
         <div className="container">
             {/* สร้างฟอรม์ */}
             <form className="form">        
             {/* Content ภายในฟอรม์ */}        
-                <h2>Registration Form</h2>
+                <h1>Registration Form</h1>
                 <div className="form-control">
                      <label>Username</label>
-                     <input type="text" />
+                     {/*ข้อมูลที่ User กรอก จะถูกเก็บใน State userName */}
+                     <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
                      <small>error massage</small>
                 </div>
                 <div className="form-control">
                      <label>Email</label>
-                     <input type="text" />
+                     <input type="text" value={email} onChange={(e)=>SetEmail(e.target.value)}/>
                      <small>error massage</small>
                 </div>
                 <div className="form-control">
                      <label>Password</label>
-                     <input type="text" />
+                     <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                      <small>error massage</small>
                 </div>
                 <div className="form-control">
                      <label>Confirm Password</label>
-                     <input type="text" />
+                     <input type="text" value={confirmpassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
                      <small>error massage</small>
                 </div>
                 <button type="submit">Register</button>
