603231d761571907cf9bc949b11f10ba86630ee3
P <dpheeraphat2547@gmail.com>
2022-12-20 19:39:52 +0700

    Create Form

---
+++ src/component/FormComponent.jsx
@@ -0,0 +1,33 @@
+const FormComponent = () => {
+    return(       
+        <div className="container">
+            {/* สร้างฟอรม์ */}
+            <form className="form">        
+            {/* Content ภายในฟอรม์ */}        
+                <div className="form-control">
+                     <label>Username</label>
+                     <input type="text" />
+                     <small>error massage</small>
+                </div>
+                <div className="form-control">
+                     <label>Email</label>
+                     <input type="text" />
+                     <small>error massage</small>
+                </div>
+                <div className="form-control">
+                     <label>Password</label>
+                     <input type="text" />
+                     <small>error massage</small>
+                </div>
+                <div className="form-control">
+                     <label>Confirm Password</label>
+                     <input type="text" />
+                     <small>error massage</small>
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default FormComponent;
