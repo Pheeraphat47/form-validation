38d570cd6cdc34aa85072fde6b59230f3a2946eb
P <dpheeraphat2547@gmail.com>
2022-12-20 20:49:50 +0700

    Add Error Message

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -6,6 +6,14 @@ const FormComponent = () => {
     const [email,SetEmail] = useState('');
     const [password,setPassword] = useState('');
     const [confirmpassword,setConfirmPassword] = useState('');
+
+
+
+    /* สร้าง State สำหรับ  Error Message*/
+    const [errorUserName , setErrorUserName] = useState('Username should be at least 8 characters.');
+    const [errorEmail,setErrorEmail] = useState('Invalid email format');
+    const [errorPassword,setErrorPassword] = useState('Password must be at least 8 characters.');
+    const [errorConfirmPassword,setErrorConfirmPassword] = useState('Password does not match');
     return(       
         <div className="container">
             {/* สร้างฟอรม์ */}
@@ -16,22 +24,22 @@ const FormComponent = () => {
                      <label>Username</label>
                      {/*ข้อมูลที่ User กรอก จะถูกเก็บใน State userName */}
                      <input type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
-                     <small>error massage</small>
+                     <small>{errorUserName}</small>
                 </div>
                 <div className="form-control">
                      <label>Email</label>
                      <input type="text" value={email} onChange={(e)=>SetEmail(e.target.value)}/>
-                     <small>error massage</small>
+                     <small>{errorEmail}</small>
                 </div>
                 <div className="form-control">
                      <label>Password</label>
                      <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/>
-                     <small>error massage</small>
+                     <small>{errorPassword}</small>
                 </div>
                 <div className="form-control">
                      <label>Confirm Password</label>
                      <input type="text" value={confirmpassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
-                     <small>error massage</small>
+                     <small>{errorConfirmPassword}</small>
                 </div>
                 <button type="submit">Register</button>
             </form>
